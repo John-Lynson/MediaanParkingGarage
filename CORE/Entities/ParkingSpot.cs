21afffb7b97dab65f39b756e318b5778f8352d69
Lin <477398@student.fontys.nl>
2024-03-12 14:00:00 +0100

    pushing work on DALL

---
+++ CORE/Entities/ParkingSpot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE.Entities
+{
+    public class ParkingSpot : BaseEntity
+    {
+        [ForeignKey("parking_garage_id")]
+        public int GarageId { get; set; }
+
+        [Column("disabled")]
+        public bool IsDisabled { get; set; }
+
+        [Column("code_name")]
+        public string CodeName { get; set; }
+    }
+}
---
+++ DALL/Repositories/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using CORE.Entities;
+using CORE.Interfaces.IRepositories;
+using DALL.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALL.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        private GarageContext _context;
+        private DbSet<T> _dbSet;
+
+        public BaseRepository(GarageContext context)
+        {
+            this._context = context;
+            this._dbSet = context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            this._dbSet.Add(entity);
+            this._context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            this._dbSet.Remove(entity);
+            this._context.SaveChanges();
+        }
+
+        public void Save(T entity)
+        {
+            this._dbSet.Update(entity);
+            this._context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return this._dbSet.Where(x => x.Id == id).First();
+        }
+    }
+}
