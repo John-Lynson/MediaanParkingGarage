8b20c99bfe39b477887097006e905f93b8a7b1d6
drm0011 <119403263+drm0011@users.noreply.github.com>
2024-03-18 12:10:18 +0100

    revised services

--- CORE/Services/RegistrationService.cs
+++ CORE/Services/RegistrationService.cs
@@ -5,11 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CORE.Interfaces.IRepositories;
-using CORE.Interfaces.IServices;
 
 namespace CORE.Services
 {
-    public class RegistrationService : IRegistrationService
+    public class RegistrationService
     {
         private readonly ICarRepository _carRepository;
 
@@ -17,32 +16,15 @@ namespace CORE.Services
         {
             _carRepository = carRepository ?? throw new ArgumentNullException(nameof(carRepository));
         }
-        public void Create(int accountId, string licensePlate)
+        public void CreatePlate(int accountId, string licensePlate)
         {
-            if (Car. == null)
-                throw new ArgumentNullException(nameof(car.LicensePlate));
-
-            if (string.IsNullOrEmpty(license.LicensePlateNumber))
-                throw new ArgumentException("License plate cannot be empty.", nameof(car.LicensePlate));
-
-            // Check if the license plate is already registered
-            var existingLicense = _carRepository.GetLicensePlate(license.LicensePlateNumber);
-            if (existingLicense != null)
-            {
-                // Update existing record, or throw an exception, depending on your requirements
-                throw new InvalidOperationException("License plate already registered.");
-            }
-            else
-            {
-                // Set entry time for new registration
-                license.EntryTime = DateTime.Now;
-                _carRepository.AddLicensePlate(license);
-            }
+            if (string.IsNullOrEmpty(licensePlate))
+                throw new ArgumentException("License plate cannot be empty.", nameof(licensePlate));
 
             Car car = new Car
             {
-                AccountId
-                LicensePlate
+                AccountId = accountId,
+                LicensePlate = licensePlate,
             };
 
             this._carRepository.Add(car);
--- ParkingGarage.Tests/RegistrationServiceTests.cs
+++ ParkingGarage.Tests/RegistrationServiceTests.cs
@@ -1,5 +1,6 @@
-﻿using CORE.Interfaces;
-using CORE.Models;
+﻿using CORE.Entities;
+using CORE.Interfaces;
+using CORE.Interfaces.IRepositories;
 using CORE.Services;
 using Moq;
 using System;
@@ -14,20 +15,19 @@ namespace ParkingGarage.Tests
     public class RegistrationServiceTests
     {
         [TestMethod]
-        public void RegisterPlate_NewLicense_ShouldRegisterSuccessfully()
+        public void CreatePlate_NewCar_ShouldCreateSuccessfully()
         {
             // Arrange
-            var mockRepository = new Mock<ILicensePlateRepository>();
-            mockRepository.Setup(repo => repo.GetLicensePlate(It.IsAny<string>())).Returns((LicensePlate)null);
-
+            var mockRepository = new Mock<ICarRepository>();
             var service = new RegistrationService(mockRepository.Object);
-            var newLicense = new LicensePlate { LicensePlateNumber = "ABC123" };
+            var accountId = 1;
+            var licensePlate = "ABC123";
 
             // Act
-            service.RegisterPlate(newLicense);
+            service.CreatePlate(accountId, licensePlate);
 
             // Assert
-            mockRepository.Verify(repo => repo.AddLicensePlate(newLicense), Times.Once);
+            mockRepository.Verify(repo => repo.Add(It.IsAny<Car>()), Times.Once);
         }
 
     }
