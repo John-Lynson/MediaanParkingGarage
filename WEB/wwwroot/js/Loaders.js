c40623044f31e24817d6d03aa08156b8d071e15c
Johnny Graanoogst <Johnlynson809@gmail.com>
2024-03-25 14:16:38 +0100

    Preloader fix

--- WEB/wwwroot/js/Loaders.js
+++ WEB/wwwroot/js/Loaders.js
@@ -1,14 +1,17 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     var preloader = document.getElementById("preloader");
+    var navbar = document.querySelector(".navbar"); // Pas deze selector aan aan jouw HTML
 
     // Check if navigating within the app
     if (sessionStorage.getItem('isNavigatingWithinApp')) {
         // Hide preloader immediately if we're navigating within the app
         preloader.style.display = 'none';
+        navbar.style.display = "flex"; // Zorg ervoor dat de navbar weer wordt getoond
     } else {
         // This is either the first load or a reload
         if (!sessionStorage.getItem('isPreloaderShown')) {
             preloader.style.display = 'block';
+            navbar.style.display = "none"; // Verberg de navbar tijdelijk
             sessionStorage.setItem('isPreloaderShown', 'true');
 
             var minPreloaderTime = 1500; // Minimale weergavetijd van de preloader in milliseconden
@@ -19,9 +22,11 @@
                 if (elapsedTime < minPreloaderTime) {
                     setTimeout(function () {
                         preloader.style.display = 'none';
+                        navbar.style.display = "flex"; // Toon de navbar weer
                     }, minPreloaderTime - elapsedTime);
                 } else {
                     preloader.style.display = 'none';
+                    navbar.style.display = "flex"; // Toon de navbar weer
                 }
             }
 
@@ -29,6 +34,7 @@
             setTimeout(hidePreloader, 5000); // Maximale weergavetijd als backup
         } else {
             preloader.style.display = 'none';
+            navbar.style.display = "flex"; // Zorg ervoor dat de navbar wordt getoond
         }
     }
 
