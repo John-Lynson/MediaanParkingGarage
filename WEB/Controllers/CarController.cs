bde3d93f12d4277401785bf03c8b7a573c72d329
Rick S <290278@student.fontys.nl>
2024-04-08 11:31:32 +0200

    reg functie wip

---
+++ WEB/Controllers/CarController.cs
@@ -0,0 +1,34 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Policy;
+using CORE.Services;
+using CORE.Entities;
+
+namespace MediaanParkingGarage.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly RegistrationService _registrationService;
+
+        public CarController(RegistrationService registrationService)
+        {
+            _registrationService = registrationService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCar([FromBody] CarRequestModel model)
+        {
+            try
+            {
+                _registrationService.CreateCar(model.AccountId, model.LicensePlate);
+                return Ok("Car created successfully.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
