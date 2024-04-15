730579604b327a46807e60a0294c3f7a296689c4
Lin <477398@student.fontys.nl>
2024-04-15 12:18:35 +0200

    added a method to get all cars linked to a specific auth0id

--- DALL/Repositories/CarRepository.cs
+++ DALL/Repositories/CarRepository.cs
@@ -7,5 +7,10 @@ namespace DALL.Repositories
     public class CarRepository : Repository<Car>, ICarRepository
     {
         public CarRepository(GarageContext context) : base(context) { }
-    }
+
+		public List<Car> GetAllByAccountId(int accountId)
+		{
+			return this._dbSet.Where(x => x.AccountId == accountId).ToList();
+		}
+	}
 }
