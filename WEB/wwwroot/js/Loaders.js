a8d3cd197a146825e31dc77cd0211e13652f332e
Johnny Graanoogst <Johnlynson809@gmail.com>
2024-05-06 14:18:36 +0200

    Aanpassingen

--- WEB/wwwroot/js/Loaders.js
+++ WEB/wwwroot/js/Loaders.js
@@ -3,29 +3,39 @@
     var navbar = document.querySelector(".navbar");
 
     function managePreloaderVisibility(hide) {
-        if (preloader && navbar) {
+        if (preloader) {
             preloader.style.display = hide ? 'none' : 'block';
+        }
+        if (navbar) {
             navbar.style.display = hide ? "flex" : "none";
         }
     }
 
+    // Beheer preloader en navbar zichtbaarheid afhankelijk van navigatie binnen de app
     if (sessionStorage.getItem('isNavigatingWithinApp')) {
         managePreloaderVisibility(true);
     } else {
+        // Markeren dat de preloader is getoond
         sessionStorage.setItem('isPreloaderShown', 'true');
         managePreloaderVisibility(false);
 
-        var minPreloaderTime = 1500;
+        // Minimale tijd dat de preloader zichtbaar moet blijven
+        var minPreloaderTime = 4500;
         var startTime = new Date().getTime();
 
         function hidePreloader() {
             var elapsedTime = new Date().getTime() - startTime;
+            // Verbergt de preloader na de minimale tijd, waarbij rekening wordt gehouden met de verstreken tijd sinds de pagina begon met laden
             setTimeout(() => managePreloaderVisibility(true), Math.max(minPreloaderTime - elapsedTime, 0));
         }
 
+        // Luister naar het 'load' event om te verzekeren dat alle pagina-inhoud is geladen voordat de preloader wordt verborgen
         window.addEventListener('load', hidePreloader);
-        setTimeout(hidePreloader, 5000); // Maximum time limit as a fallback
+
+        // Fallback, verbergt de preloader na een maximale tijdlimiet, ongeacht of de pagina volledig geladen is
+        setTimeout(hidePreloader, 9000); // Maximale tijdlimiet als backup
     }
 
+    // Marker voor toekomstige navigaties binnen de app
     sessionStorage.setItem('isNavigatingWithinApp', 'true');
 });
