072407a49abc392ad2bdb233f0198be819754453
Lin Lin <477398@student.fontys.nl>
2024-05-07 11:20:42 +0200

    made it possible to run a mock database (in your RAM)

--- WEB/Program.cs
+++ WEB/Program.cs
@@ -9,53 +9,57 @@ using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
 
 namespace WEB
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-            // Auth0 configuratie
-            builder.Services.AddAuth0WebAppAuthentication(options =>
-            {
-                options.Domain = builder.Configuration["Auth0:Domain"];
-                options.ClientId = builder.Configuration["Auth0:ClientId"];
-            });
+			// Auth0 configuratie
+			builder.Services.AddAuth0WebAppAuthentication(options =>
+			{
+				options.Domain = builder.Configuration["Auth0:Domain"];
+				options.ClientId = builder.Configuration["Auth0:ClientId"];
+			});
 
-            // Voeg dit toe als je MVC controllers en views gebruikt
-            builder.Services.AddControllersWithViews(); // Belangrijk voor MVC
+			// Voeg dit toe als je MVC controllers en views gebruikt
+			builder.Services.AddControllersWithViews(); // Belangrijk voor MVC
 
-            // Als je applicatie ook autorisatiebeleid gebruikt
-            builder.Services.AddAuthorization();
+			// Als je applicatie ook autorisatiebeleid gebruikt
+			builder.Services.AddAuthorization();
 
-            // Database initialization
-            SqlConnectionStringBuilder conBuilder = new SqlConnectionStringBuilder();
+			// Database initialization
+			SqlConnectionStringBuilder conBuilder = new SqlConnectionStringBuilder();
 
-            builder.Services.AddDbContext<GarageContext>(options => options.UseSqlServer("Data Source=10.0.3.10;Database=mediaan_parking_garage;User ID=SA;Password=Software01!;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False"));
+			builder.Services.AddDbContext<GarageContext>(options =>
+				{
+					// options.UseSqlServer("Data Source=10.0.3.10;Database=mediaan_parking_garage;User ID=SA;Password=Software01!;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+					options.UseInMemoryDatabase("MockGarageDB");
+				});
 
-            // Build
-            var app = builder.Build();
+			// Build
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+			app.UseAuthentication();
+			app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
